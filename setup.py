bf6b79ea435be73abf7b4f88fc0c9f803786591f
Alice Bevan-McGregor <alice@gothcandy.com>
2017-08-14 02:06:25 -0400

    Version bump, metadata update.

--- setup.py
+++ setup.py
@@ -7,20 +7,15 @@ import os
 import sys
 import codecs
 
+from setuptools import setup
 
-try:
-	from setuptools.core import setup, find_packages
-except ImportError:
-	from setuptools import setup, find_packages
-
-
-if sys.version_info < (2, 7):
-	raise SystemExit("Python 2.7 or later is required.")
-elif sys.version_info > (3, 0) and sys.version_info < (3, 2):
-	raise SystemExit("Python 3.2 or later is required.")
+if sys.version_info < (3, 3):
+	raise SystemExit("Python 3.3 or later is required.")
 
 version = description = url = author = None
+
 exec(open(os.path.join("web", "dispatch", "resource", "release.py")).read())
+
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -28,9 +23,8 @@ tests_require = [
 		'pytest',  # test collector and extensible runner
 		'pytest-cov',  # coverage reporting
 		'pytest-flakes',  # syntax validation
-		'pytest-capturelog',  # log capture
-		'pytest-spec',  # output formatting
-		'WebCore',  # request mocking
+		'pytest-catchlog',  # log capture
+		'WebOb',  # request mocking
 	]
 
 
@@ -44,7 +38,7 @@ setup(
 	author = author.name,
 	author_email = author.email,
 	license = 'MIT',
-	keywords = ['web.dispatch', 'WebCore', 'REST'],
+	keywords = ['marrow', 'dispatch', 'web.dispatch', 'resource dispatch', 'endpoint discovery', 'WebCore', 'REST'],
 	classifiers = [
 			"Development Status :: 5 - Production/Stable",
 			"Environment :: Console",
@@ -53,42 +47,43 @@ setup(
 			"License :: OSI Approved :: MIT License",
 			"Operating System :: OS Independent",
 			"Programming Language :: Python",
-			"Programming Language :: Python :: 2",
-			"Programming Language :: Python :: 2.7",
 			"Programming Language :: Python :: 3",
-			"Programming Language :: Python :: 3.2",
 			"Programming Language :: Python :: 3.3",
 			"Programming Language :: Python :: 3.4",
 			"Programming Language :: Python :: 3.5",
+			"Programming Language :: Python :: 3.6",
 			"Programming Language :: Python :: Implementation :: CPython",
 			"Programming Language :: Python :: Implementation :: PyPy",
 			"Topic :: Software Development :: Libraries :: Python Modules",
 		],
 	
-	packages = find_packages(exclude=['bench', 'docs', 'example', 'test']),
+	packages = ['web.dispatch.resource'],
 	include_package_data = True,
-	namespace_packages = [
-			'web',  # primary namespace
-			'web.dispatch',  # extensible dispatch mechanisms
+	package_data = {'': ['README.rst', 'LICENSE.txt']},
+	zip_safe = False,
+	
+	# Dependency Declaration
+	
+	setup_requires = [
+			'pytest-runner',
+		] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else [],
+	
+	install_requires = [
+			'web.dispatch~=3.0.1',  # Core dispatch helpers.
+			'pathlib; python_version < "3.4"',  # Path manipulation utility lib; builtin in 3.4 and 3.5.
 		],
+	
+	extras_require = dict(
+			development = tests_require + ['pre-commit'],  # Development-time dependencies.
+		),
+	
+	tests_require = tests_require,
+	
+	# Plugin Registration
 	
 	entry_points = {
 			'web.dispatch': [
 					'resource = web.dispatch.resource:ResourceDispatch',  # REST resource dispatch
 				],
 		},
-	
-	setup_requires = [
-			'pytest-runner',
-		] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else [],
-	install_requires = [
-		],
-	
-	extras_require = dict(
-			development = tests_require,
-		),
-	
-	tests_require = tests_require,
-	
-	zip_safe = True,
 )
